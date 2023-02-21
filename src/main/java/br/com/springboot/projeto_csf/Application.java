7359037869031d4727c2d0a730b6eadb73a7b784
Claudivan <ivamsferreira@gmail.com>
2023-02-21 12:01:03 -0300

    Iniciando o banco de dados

--- src/main/java/br/com/springboot/projeto_csf/Application.java
+++ src/main/java/br/com/springboot/projeto_csf/Application.java
@@ -2,11 +2,14 @@ package br.com.springboot.projeto_csf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 /**
  *
  * Spring Boot application starter class
  */
+
+@EntityScan(basePackages = "br.com.springboot.projeto_csf.model")
 @SpringBootApplication
 public class Application { 
 	
