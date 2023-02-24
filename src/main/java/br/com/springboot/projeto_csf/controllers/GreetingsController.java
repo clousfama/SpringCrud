fc7ef1149a9b99a0c1f7fafbf8da31c7b063676e
Claudivan <ivamsferreira@gmail.com>
2023-02-24 11:41:43 -0300

    finalizando os end-points

--- src/main/java/br/com/springboot/projeto_csf/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/projeto_csf/controllers/GreetingsController.java
@@ -1,17 +1,24 @@
 package br.com.springboot.projeto_csf.controllers;
 
+import java.util.List;
+
+import org.apache.tomcat.jni.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import antlr.collections.java.util.List;
 import br.com.springboot.projeto_csf.model.Usuario;
 import br.com.springboot.projeto_csf.repository.UsuarioRepository;
 
@@ -27,11 +34,7 @@ public class GreetingsController {
 	
 	
 	
-    /**
-     *
-     * @param name the name to greet
-     * @return greeting text
-     */
+    
     @RequestMapping(value = "/{name}", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     public String greetingText(@PathVariable String name) {
@@ -51,14 +54,70 @@ public class GreetingsController {
     
     @GetMapping(value = "listatodos") /*Nosso primeiro método de API*/
     @ResponseBody /*Retorna os dados para o corpo da resposta*/
-    public ResponseEntity<java.util.List<Usuario>> listaUsuario(){ 
+    public ResponseEntity<List<Usuario>> listaUsuario(){ 
     	
-    	java.util.List<Usuario> usuarios = usuarioRepository.findAll(); /*Executa a consulta no banco de dados*/
+    	List<Usuario> usuarios = usuarioRepository.findAll(); /*Executa a consulta no banco de dados*/
     	
     	return new ResponseEntity<java.util.List<Usuario>>(usuarios, HttpStatus.OK); /*Retorna uma lista em JSON*/ 	
     	   	
       			
 	}
+   
+    @PostMapping(value = "salvar")
+    @ResponseBody
+    public ResponseEntity<Usuario> salvar (@RequestBody Usuario usuario){
+    	
+    	Usuario user = usuarioRepository.save(usuario);
+    	
+    	return new ResponseEntity<Usuario>(user, HttpStatus.CREATED);
+    	
+    }
+    
+    @PutMapping(value = "atualizar")
+    @ResponseBody
+    public ResponseEntity<?> atualizar (@RequestBody Usuario usuario){
+    	
+    	if (usuario.getId()== null) {
+    		return new ResponseEntity<String>("Id não foi informado para atualização", HttpStatus.OK);
+			
+		}
+    	
+    	Usuario user = usuarioRepository.saveAndFlush(usuario);
+    	
+    	return new ResponseEntity<Usuario>(user, HttpStatus.OK);
+    	
+    }
+    
+    
+    @DeleteMapping(value = "delete")
+    @ResponseBody
+    public ResponseEntity<String> delete (@RequestParam Long iduser){
+    	
+    	usuarioRepository.deleteById(iduser);
+    	
+    	return new ResponseEntity<String>("User deletado com sucesso", HttpStatus.OK);
+    	
+    }
+    
+    @GetMapping(value = "buscaruserid")
+    @ResponseBody
+    public ResponseEntity<Usuario> buscaruserid(@RequestParam(name = "iduser")Long iduser) {
+    	
+    	Usuario usuario = usuarioRepository.findById(iduser).get();
+    	
+    	return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
+    	
+    }
+    
+    @GetMapping(value = "buscarPorNome")
+    @ResponseBody
+    public ResponseEntity<List<Usuario>> buscarPorNome(@RequestParam(name = "name")String name) {
+    	
+    	List<Usuario> usuario = usuarioRepository.buscarPorNome(name.trim().toUpperCase());
+    	
+    	return new ResponseEntity<List<Usuario>>(usuario, HttpStatus.OK);
+   
+    }
     
     
 }
